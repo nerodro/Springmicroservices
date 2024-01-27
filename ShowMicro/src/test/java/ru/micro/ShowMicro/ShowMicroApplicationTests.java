9ee649f6d21a9069bf0f87553ecc5207bc7eee96
никита тараканов <ntarak2002@gmail.com>
2024-01-27 15:58:26 +0300

    NewService

--- ShowMicro/src/test/java/ru/micro/ShowMicro/ShowMicroApplicationTests.java
+++ ShowMicro/src/test/java/ru/micro/ShowMicro/ShowMicroApplicationTests.java
@@ -1,11 +1,17 @@
 package ru.micro.ShowMicro;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class ShowMicroApplicationTests {
-
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+			"postgres:15-alpine"
+	);
 	@Test
 	void contextLoads() {
 	}
