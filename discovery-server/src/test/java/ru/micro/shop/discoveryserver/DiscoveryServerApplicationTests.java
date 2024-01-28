cc1ad36ddd5dd4cc9a7debc9f430ed0d10f0b020
никита тараканов <ntarak2002@gmail.com>
2024-01-28 16:12:25 +0300

    VeryBigUpdate

---
+++ discovery-server/src/test/java/ru/micro/shop/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.micro.shop.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
