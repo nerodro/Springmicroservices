9bc68792e700b9d68f7b1428a11fba17f9613b55
никита тараканов <ntarak2002@gmail.com>
2024-01-29 17:43:25 +0300

    Gateway

---
+++ GateWay/src/test/java/ru/micro/shop/GateWay/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.micro.shop.GateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
